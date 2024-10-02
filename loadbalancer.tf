98836e437a07e68c52b8e78ef0827e4ea5f98791
Ken Sun <ken.sun@ssc-spc.gc.ca>
2024-10-02 09:48:53 -0400

    update with load balancer module

--- loadbalancer.tf
+++ loadbalancer.tf
@@ -79,3 +79,20 @@ resource "azurerm_lb_rule" "loadbalancer-lbr" {
   disable_outbound_snat          = try(each.value.disable_outbound_snat, null)
   enable_tcp_reset               = try(each.value.enable_tcp_reset, null)
 }
+
+module "load_balancer" {
+  count = try(var.vmss.lb, null) != null ? 1 : 0
+  source   = "github.com/canada-ca-terraform-modules/terraform-azurerm-caf-load_balancer.git"
+
+  location          = var.location
+  subnets           = var.subnets
+  resource_groups   = var.resource_groups
+  userDefinedString = var.userDefinedString
+  tags              = var.tags
+  env               = var.env
+  group             = var.group
+  project           = var.project
+  load_balancer      = var.vmss.lb
+  custom_data       = try(var.vmss.lb.custom_data, false) != false ? base64encode(file("${path.cwd}/${var.vmss.lb.custom_data}")) : null
+  user_data         = try(var.vmss.lb.user_data, false) != false ? base64encode(file("${path.cwd}/${var.vmss.lb.user_data}")) : null
+}
