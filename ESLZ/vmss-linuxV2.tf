463bae2d579e59eb25b40093c0aaa12a2cbd1053
bernardmaltais <bernard.maltais@ssc-spc.gc.ca>
2024-11-13 12:06:16 -0500

    Update vmss-linuxV2.tf

--- ESLZ/vmss-linuxV2.tf
+++ ESLZ/vmss-linuxV2.tf
@@ -6,7 +6,7 @@ variable "vmss_linuxV2" {
 
 module "vmss_linuxV2" {
   for_each = var.vmss_linuxV2
-  source   = "github.com/canada-ca-terraform-modules/terraform-azurerm-caf-vmss-linuxV2?ref=v1.0.6"
+  source   = "github.com/canada-ca-terraform-modules/terraform-azurerm-caf-vmss-linuxV2?ref=v1.0.7"
 
   location          = var.location
   subnets           = local.subnets
